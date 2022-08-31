1245339d76cb6421147a1208311df9df8494cb08
cjcar61 <79336360+cjcar61@users.noreply.github.com>
2022-08-30 23:43:20 -0300

    project 1 requirements fullfilled

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // the javascript for project 1
 $(document).ready(function(){
     $("#burger").click(function(){
-        // $(".dropdown").toggleClass("show");
-        $(".dropdown").toggle(1000);
+         $(".dropdown").toggleClass("show");
+        //$(".dropdown").toggle(1000);
     });
   });
   
